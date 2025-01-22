d48d5ace131f22d189327f4ee594af570484bb5c
abdul-luh <abdulluhodulate@gmail.com>
2025-01-22 05:23:14 +0100

    initate sign-up and sign-in page layout

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import AuthForm from "@/components/AuthForm";
 
 export default function page() {
   return (
-    <div>sign-in</div>
-  )
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type={"sign-in"} />
+    </section>
+  );
 }
---
+++ components/AuthForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AuthForm({ type }: AuthFormProps) {
+  return (
+    <div>
+      {type}
+      AuthForm
+    </div>
+  );
+}
