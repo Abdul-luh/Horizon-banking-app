a26748360e8604cf2cfae72c14a83fff6a9009eb
abdul-luh <abdulluhodulate@gmail.com>
2025-01-22 18:15:55 +0100

    add form initial form template

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,6 @@
 import AuthForm from "@/components/AuthForm";
 
-export default function page() {
+export default function SignUp() {
   return (
     <section className="flex-center size-full max-sm:px-6">
       <AuthForm type={"sign-up"} />
