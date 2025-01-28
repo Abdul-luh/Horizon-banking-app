49dbdd788c53a36999e77ea64c7b6fc308f4ec07
abdul-luh <abdulluhodulate@gmail.com>
2025-01-28 16:30:53 +0100

    add a create user acount function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 "use server";
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
 export async function signIn() {
   try {
     // Mutations / Database /Make fetch
@@ -8,10 +13,36 @@ export async function signIn() {
   }
 }
 
-export async function signUp({ userData }: SignUpParams) {
+export async function signUp(userData: SignUpParams) {
   try {
-    // CREATE A USER ACCOUNT
+    const { email, firstName, lastName, password } = userData;
+    const { account } = await createAdminClient();
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.log(error);
+  }
+}
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return account.get();
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return null;
   }
 }
